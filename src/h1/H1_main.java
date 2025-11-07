56fd7c224153106239d303781e0caa0cbf32a837
Jolina Schennen <jschenne@smail.uni-koeln.de>
2025-11-07 13:06:27 +0100

    added task first try

---
+++ src/h1/H1_main.java
@@ -0,0 +1,28 @@
+package h1;
+
+public class H1_main {
+	public static void main(String[] args) {
+		double guthaben=-100, monEingang=200;
+		int rating=-2;
+		boolean warnhinweis=true, negativ=false;
+	if (guthaben<0) {
+		if (monEingang>=guthaben) {
+			rating +=1;
+		}
+		if (monEingang<guthaben) {
+			rating -= 1;
+			if (rating<0) {
+				warnhinweis=true;
+			} else warnhinweis=false;
+		}
+		negativ=true;
+	} else negativ=false;
+	if (guthaben>0) {
+		rating += 3;
+	} 
+	if (guthaben==0) {	
+		rating += 2;
+	}
+	System.out.println("guthaben:"+guthaben+"monEingang:"+monEingang+"rating"+rating+"warnhinweis"+warnhinweis+"negativ"+negativ);
+	} 
+}
